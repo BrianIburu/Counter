f87c7e29b2c6ae76290790493b6a37e7caee434a
BrianIburu <brayocoder001@gmail.com>
2025-11-29 00:01:33 +0300

    Created a counter

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import React,{useState} from "react";
+function Counter(){
+    const [count,setCount]=useState(0);
+    const Increment = ()=>{
+        setCount(count+1);
+    }
+    const Decrement = ()=>{
+        setCount(count-1);
+    }
+    const Reset = () => {
+        setCount(0);
+    }
+    return(
+        <div>
+            <p>{count}</p>
+            <button onClick={Reset}>Reset</button>
+            <button onClick={Increment}>Increment</button>
+            <button onClick={Decrement}>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
